a80fd27e8c8ea9d4ff92b45f43fa9ab4c433c8c3
safwan158 <safwannoon@gmail.com>
2021-09-05 21:50:18 +0000

    Making changes to mongodb script

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -8,7 +8,7 @@ baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-if [ ?0 -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
 else
     echo -e "\e[31mFAILURE\e[0m"
@@ -16,14 +16,14 @@ fi
 
 echo "Installing MongoDB"
 yum install -y mongodb-org &>>/tmp/log
-if [ ?0 -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
 else
     echo -e "\e[31mFAILURE\e[0m"
 fi
 
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-if [ ?0 -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
 else
     echo -e "\e[31mFAILURE\e[0m"
@@ -32,14 +32,14 @@ fi
 echo "Starting MongoDB" 
 systemctl enable mongod
 systemctl restart mongod
-if [ ?0 -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
 else
     echo -e "\e[31mFAILURE\e[0m"
 fi
 echo "Downloading MongoDB"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
-if [ ?0 -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
 else
     echo -e "\e[31mFAILURE\e[0m"
@@ -50,14 +50,14 @@ echo "Downloading MongoDB schema"
 unzip -o mongodb.zip &>>/tmp/log
 cd mongodb-main
 echo "Loading Schema"
-if [ ?0 -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
 else
     echo -e "\e[31mFAILURE\e[0m"
 fi
 mongo < catalogue.js &>>/tmp/log
 mongo < users.js &>>/tmp/log
-if [ ?0 -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
 else
     echo -e "\e[31mFAILURE\e[0m"
