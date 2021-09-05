4255bb1f0929091204762ee3aad0cc699287173d
safwan158 <safwannoon@gmail.com>
2021-09-05 22:26:05 +0000

    Making changes to mongodb script

---
+++ roboshop/components/common.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+Status_Check() {
+    if [ $1 -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 2
+fi    
+    
+}
+Print(){
+    echo -n -e "$1 \t - "
+}
+if [ $UID -ne 0 ]; then
+    echo -e "\e[1;33mYou Should execute this script as a root user"
+    exit 1
+fi
--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-Status_Check() {
-    if [ $1 -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-else
-    echo -e "\e[31mFAILURE\e[0m"
-    exit 2
-fi    
-    
-}
-Print(){
-    echo -n -e "$1 \t - "
-}
+source components/common.sh
 Print "Setting Up MongoDB Repo\t\t"
 
 echo '[mongodb-org-4.2]
