4e9991299ca29f89809f86069e9a0445029fa09c
safwan158 <safwannoon@gmail.com>
2021-09-08 03:07:06 +0000

    Making changes to the Redis Script

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -14,11 +14,11 @@ Print "Install Redis\t\t\t\t"
 yum install redis -y &>>$LOG
 Status_Check $?
 
-Print "Configure Redis Listen Address"
+Print "Configure Redis Listen Address\t\t"
 sed -i -e 's/127.0.0.1/0.0.0/' /etc/redis.conf
 Status_Check $?
 
-Print "Start Redis Service"
+Print "Start Redis Service\t\t\t"
 systemctl enable redis &>>$LOG && systemctl start redis &>>$LOG
 Status_Check $?
 
