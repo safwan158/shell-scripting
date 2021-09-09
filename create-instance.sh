b339e6250f4a998f32a40242972da9c9dc32377b
safwan158 <safwannoon@gmail.com>
2021-09-09 03:42:35 +0000

    Update Create Instance script

--- create-instance.sh
+++ create-instance.sh
@@ -9,6 +9,13 @@ if [ -z "${INSTANCE_NAME}" ]; then
     exit 1
 fi
 
+aws ec2 describe-instances --filters "Name=tag:Name,Values=catalogue" | jq .Rerservations[].Intances[].State.Name | grep running &>/dev/null
+if [ $?- eq 0 ]; then 
+    echo "Instance $INSTANCE_NAME is already running"
+    exit 0
+fi
+
+
 IP=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "ResourceType=spot-instances-request,
-Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" | jq .Instances[].PrivateIpAddress | sed -e 
+Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" | jq .Instances[].PrivateIpAddress | sed -e
 's/"//g')
