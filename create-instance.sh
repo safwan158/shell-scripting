8aecae2a9c709303f77c82347194e1d095719d49
safwan158 <safwannoon@gmail.com>
2021-09-09 02:30:24 +0000

    Updating the create Instance

--- create-instance.sh
+++ create-instance.sh
@@ -3,4 +3,4 @@
 LID="lt-0c74196d9abfd39e2"
 LVER=1
 
-aws ec2 run-instances --launch-template LaunchTemplate=$LID, Version=$LVER | jq
+aws ec2 run-instances --launch-template LaunchTemplate=$LID,Version=$LVER | jq
