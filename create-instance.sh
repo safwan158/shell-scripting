e5834b66000a60eba0afd24553a68d43203f7afd
safwan158 <safwannoon@gmail.com>
2021-09-09 03:23:42 +0000

    Update Create Instance script

--- create-instance.sh
+++ create-instance.sh
@@ -10,5 +10,5 @@ if [ -z "${INSTANCE_NAME}" ]; then
 fi
 
 IP=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "ResourceType=spot-instances-request,
-Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" 
-| jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
+Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" | jq .Instances[].PrivateIpAddress | sed -e 
+'s/"//g')
