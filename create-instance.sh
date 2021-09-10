4ad8248f96f27478c179ba27ef4a516a68c4ebbd
Safwan Noon <safwannoon@gmail.com>
2021-09-10 02:58:51 +0000

    Create Instance script

--- create-instance.sh
+++ create-instance.sh
@@ -22,3 +22,6 @@ if [ $? -eq 0 ]; then
 fi
 
 IP=$(aws ec2 run-instances --launch-template LaunchTemplateId=$LID,Version=$LVER --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME}]" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
+
+sed -e "s/INSTANCE_NAME/$INSTANCE_NAME/" -e "s/INSTANCE_IP/$IP" record.json >/tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id Z074624316T6EULY60B1C --change-batch file://tmp/record.json | jq
