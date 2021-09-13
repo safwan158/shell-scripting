c7ba5c4b405aa0a916d08799264674e0d6972da9
Safwan158 <safwannoon@gmail.com>
2021-09-13 23:52:31 +0000

    Updating the frontend script

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -43,7 +43,7 @@ DOWNLOAD() {
 }
 SystemD-Setup() {
     Print "Update SystemD Service\t\t"
-    sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal' /home/roboshop/${COMPONENT}/systemd.service
+    sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e  's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service
     Status_Check $?
     
     Print "SetUp SystemD Service\t\t"
