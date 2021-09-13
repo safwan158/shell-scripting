a60d3a63016f72f08b1937c58174f3348f566bf4
Safwan158 <safwannoon@gmail.com>
2021-09-13 15:18:08 +0000

    Update the frontend script

---
+++ roboshop/components/frontend.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source components/common.sh
+
+Print "Install Nginx"
+yum install nginx -y &>>$LOG
+Status_Check $?
+
+Print "Download frontend Archives"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
+Status_Check $?
+
+Print "Extract frontend archive"
+rm -rf * /usr/share/nginx/html && cd /usr/share/nginx/html && unzip /tmp/frontend.zip &>>$LOG && mv frontend-main/* . && mv static/* .
+Status_Check $?
+
+Print "Update frontend Nginx Roboshop Config"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
+Status_Check $?
+
+Print "Restart Nginx\t\t"
+systemctl restart nginx &>>$LOG
+Status_Check $?
