b82e91680f099e60a3a50a4bb98ae0d57153e43b
Safwan158 <safwannoon@gmail.com>
2021-09-14 17:07:17 +0000

    Update the mysql script

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -18,7 +18,7 @@ Print "Start MySQL Service"
 systemctl enable mysqld && systemctl start mysqld &>>$LOG
 
 DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
-echo ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';  >/tmp/reset.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';"  >/tmp/reset.sql
 mysql --connect-expired-password -u root -p "${DEFAULT_PASSWORD}" </tmp/reset.sql
 exit
 
